d79d26c1812d026e036e89a2c797b629c9196cfd
Mineev Semen <semenmineev@gmail.com>
2019-01-21 16:58:30 +0300

    123
    123

--- task1.py
+++ task1.py
@@ -1,22 +1,23 @@
 
-def adv_print(text, start='', max_line=6, in_file=False):
+def adv_print(*args, start='', sep='\n', max_line=6, in_file=False):
 
-    if len(start + text) > max_line:
-        result = (start + text)[:max_line] + '\n'
-        print_line = start + text
-        while len(print_line) >= max_line:
-            print_line = print_line[max_line:]
-            result += print_line[:max_line] + '\n'
-        print(result)
-        if in_file:
-            with open('file.txt', 'w', encoding='utf-8') as f:
-                f.write(result)
-    else:
-        result = start + text
-        print(result)
-        if in_file:
-            with open('file.txt', 'w', encoding='utf-8') as f:
-                f.write(result)
+    for text in args:
+        if len(start + text) > max_line:
+            result = (start + text)[:max_line] + '\n'
+            print_line = start + text
+            while len(print_line) >= max_line:
+                print_line = print_line[max_line:]
+                result += print_line[:max_line] + '\n'
+            print(result + sep)
+            if in_file:
+                with open('file.txt', 'a', encoding='utf-8') as f:
+                    f.write(result)
+        else:
+            result = start + text + sep
+            print(result)
+            if in_file:
+                with open('file.txt', 'a', encoding='utf-8') as f:
+                    f.write(result)
 
 
-adv_print('123456789', max_line=9, in_file=True)
+adv_print('1234567891', '123456789', '123456789', sep='\n', start='new', max_line=10, in_file=True)
